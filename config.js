1f0e2fa96b4d13df589698001ddca56096e4954b
xSVIP <76807561+xSVIP@users.noreply.github.com>
2022-04-02 21:55:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  title: '王同学的数码小圈', // 网站标题
+  nickName: "王同学", // 昵称
+  githubName: 'xSVIP', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
+  SEO_description: '王同学的数码小圈，欢迎来到我的小站！',
   SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc" // cloudflare的webanalyze
 }
